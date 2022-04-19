da04f35f6665784f0b7361c9cad145bf54408afe
Martin_Kracman <martin.kracman@tul.cz>
2022-04-19 21:22:00 +0200

    přidání třídy pro stažení html

--- Projekt/Klient/Form1.cs
+++ Projekt/Klient/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -20,6 +22,45 @@ namespace Klient
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private String getHtmlFromServer(string serverAdress)
+        {
+            try
+            {
+                String queryString = "user=myUser&pwd=myPassword&tel=+123456798&msg=My message";
+                byte[] requestByte = Encoding.Default.GetBytes(queryString);
+
+                // build our request
+                WebRequest webRequest = WebRequest.Create(serverAdress);
+                webRequest.Method = "POST";
+                webRequest.ContentType = "application/xml";
+                webRequest.ContentLength = requestByte.Length;
+
+                // create our stream to send
+                Stream webDataStream = webRequest.GetRequestStream();
+                webDataStream.Write(requestByte, 0, requestByte.Length);
+
+                // get the response from our stream
+                WebResponse webResponse = webRequest.GetResponse();
+                webDataStream = webResponse.GetResponseStream();
+
+                // convert the result into a String
+                StreamReader webResponseSReader = new StreamReader(webDataStream);
+                String responseFromServer = webResponseSReader.ReadToEnd().Replace("\n", "").Replace("\t", "");
+
+                // close everything
+                webResponseSReader.Close();
+                webResponse.Close();
+                webDataStream.Close();
+
+                // You now have the HTML in the responseFromServer variable, use it :)
+                return responseFromServer;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
         }
     }
 }
